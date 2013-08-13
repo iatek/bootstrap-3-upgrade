ccda65ae7d63631ea7a9a323c42fd1a6dde89eeb
Carol Skelly <carol@iatek.com>
2013-08-13 13:12:10 -0400

    more updates

---
+++ static/carbon.js
@@ -0,0 +1,1 @@
+var z = document.createElement("script"); z.type = "text/javascript"; z.async = true; z.src = "http://engine.carbonads.com/z/32644/azcarbon_2_1_0_HORIZ"; var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(z, s);
