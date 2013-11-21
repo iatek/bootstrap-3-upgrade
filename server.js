d7093eacc865aa683558ecd26f648fd13d10c7d0
Carol Skelly <carol@iatek.com>
2013-11-21 07:59:50 -0500

    update links

--- server.js
+++ server.js
@@ -54,10 +54,13 @@ var rules = [
     {name:"brand",regex:/(?!class=\")brand/g,rep:"navbar-brand"},
     {name:"btn",regex:/(?!class=\")btn btn-navbar/g,rep:"navbar-toggle"},
     {name:"nav",regex:/nav-collapse/g,rep:"navbar-collapse"},
+    {name:"nav",regex:/navbar-search/g,rep:"navbar-form"},
     {name:"toggle",regex:/nav-toggle/g,rep:"navbar-toggle"},
     {name:"util",regex:/(?!class=\")-phone/g,rep:"-sm"},
     {name:"util",regex:/(?!class=\")-tablet/g,rep:"-md"},
-    {name:"util",regex:/(?!class=\")-desktop/g,rep:"-lg"}
+    {name:"util",regex:/(?!class=\")-desktop/g,rep:"-lg"},
+    //{name:"nav",regex:/navbar-inner/g,rep:"container"},
+    //{name:"nav",regex:/navbar\s.nav/g,rep:"navbar-nav"}
     ];
 
 var convert = function(str){
@@ -74,60 +77,60 @@ var convert = function(str){
     
     if (convertNavs) {
     
-    $('#ele').remove();
-    var ele = $('<form id="ele"></form>');
-    ele.empty().html(str);
-    ele.appendTo("body");
-    
-    //navbar structure
-    var nb = $('#ele').find('.navbar');
-    if (typeof nb!="undefined"){
-        nb.addClass('navbar-default');
-        nb.find('.nav').addClass('navbar-nav'); // add the navbar-nav class
-        nb.find('.btn').addClass('navbar-btn');
-        var nb_inner = nb.find('.navbar-inner'); // remove the 2.x navbar-inner
-        var h = nb_inner.html();
+        $('#ele').remove();
+        var ele = $('<form id="ele"></form>');
+        ele.empty().html(str);
+        ele.appendTo("body");
         
-        if (typeof h!="undefined"){
-            nb.html(h);
-        }
-        
-        //wrap the brand and nav-toggle with nav-header
-        var brand = nb.find(".navbar-brand");
-        var togg = nb.find(".navbar-toggle");
-        var navbarheader = $('<div class="navbar-header"></div>');
-        
-        if (typeof brand != "undefined" && typeof togg != "undefined") {
-            brand.appendTo(navbarheader);
-            togg.appendTo(navbarheader);
-            navbarheader.prependTo(nb);
-        }
-    }
-    
-    //modal structure
-    var md = $('#ele').find('.modal');
-    if (typeof md!="undefined"){
-        md.removeClass("hide");
-        var mdia = $('<div class="modal-dialog"></div>');
-        var mc = $('<div class="modal-content"></div>');
-        mc.appendTo(mdia);
-        $('#ele').find(".modal-header").appendTo(mc);
-        $('#ele').find(".modal-body").appendTo(mc);
-        $('#ele').find(".modal-footer").appendTo(mc);
-        mdia.appendTo(md);
-        console.log("modal ele:"+ele.html());
-    }
-    
-    //icons
-    var icos = ele.find("[class^=icon]");
-    
-    //imgs
-    var imgs = ele.find('img').addClass("img-responsive");
-    
-    // replace .thumbnails with .media-list
+        //navbar structure
+        var nb = $('#ele').find('.navbar');
+        if (typeof nb!="undefined"){
+            nb.addClass('navbar-default');
+            nb.find('.nav').addClass('navbar-nav'); // add the navbar-nav class
+            nb.find('.btn').addClass('navbar-btn');
+            var nb_inner = nb.find('.navbar-inner'); // remove the 2.x navbar-inner
+            var h = nb_inner.html();
             
-    str = ele.html();
-    $('#ele').remove();
+            if (typeof h!="undefined"){
+                nb.html(h);
+            }
+            
+            //wrap the brand and nav-toggle with nav-header
+            var brand = nb.find(".navbar-brand");
+            var togg = nb.find(".navbar-toggle");
+            var navbarheader = $('<div class="navbar-header"></div>');
+            
+            if (typeof brand != "undefined" && typeof togg != "undefined") {
+                brand.appendTo(navbarheader);
+                togg.appendTo(navbarheader);
+                navbarheader.prependTo(nb);
+            }
+        }
+        
+        //modal structure
+        var md = $('#ele').find('.modal');
+        if (typeof md!="undefined"){
+            md.removeClass("hide");
+            var mdia = $('<div class="modal-dialog"></div>');
+            var mc = $('<div class="modal-content"></div>');
+            mc.appendTo(mdia);
+            $('#ele').find(".modal-header").appendTo(mc);
+            $('#ele').find(".modal-body").appendTo(mc);
+            $('#ele').find(".modal-footer").appendTo(mc);
+            mdia.appendTo(md);
+            console.log("modal ele:"+ele.html());
+        }
+        
+        //icons
+        var icos = ele.find("[class^=icon]");
+        
+        //imgs
+        var imgs = ele.find('img').addClass("img-responsive");
+        
+        // replace .thumbnails with .media-list
+                
+        str = ele.html();
+        $('#ele').remove();
     
     }
       
